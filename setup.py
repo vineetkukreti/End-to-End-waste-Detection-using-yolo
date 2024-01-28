c41986ee5b477d6eddc0cc242435e299b789b388
vineetkukreti34@gmail.com <vineetkukreti34@gmail.com>
2024-01-28 20:57:50 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Vineet Kukreti',
+    author_email= 'vineetkukreti34@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
